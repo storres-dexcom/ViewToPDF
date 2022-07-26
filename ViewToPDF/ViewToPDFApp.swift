938563836622597d1553161632a847adb3b970fa
Shayne Torres <st0720@dexcom.com>
2022-07-26 15:07:12 -0700

    Initial Commit

---
+++ ViewToPDF/ViewToPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewToPDFApp.swift
+//  ViewToPDF
+//
+//  Created by Shayne Torres on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewToPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
